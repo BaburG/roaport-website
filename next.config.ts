3aa101b793bb10425d7cef30c25fd8bf1a21376c
Ata AKSOY <ataaaksoy@gmail.com>
2024-12-25 15:29:42 +0300

    view page fetch from db - todo: images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // images: {
+  //   loader: 'custom',
+  //   loaderFile: './src/lib/imageLoader.ts',
+  // },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { PostGrid } from "@/components/ui/post-grid"
 
 export default function Home() {
+
   return (
     <main className="min-h-screen bg-background">
       <PostGrid />
